ec261e216239a4c418e78be50483df58db5892ca
ootami <ootani-yoshiyuki19@iamas.ac.jp>
2023-09-18 02:41:12 +0900

    style fixed

--- components/NeumoCard.tsx
+++ components/NeumoCard.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 
-
 type NeumoCardProps = {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
+  small?: boolean; // small プロパティを追加
 };
 
 const NeumoCard: React.FC<NeumoCardProps> = ({ children, small }) => {
-    const cardClassName = `bg-glass-bg rounded-xl shadow-glass backdrop-blur border border-neumo-border ${small ? 'p-2.5 m-2 ' : 'p-6'}`;
-  
-    return (
-      <div className={cardClassName}>
-        {children}
-      </div>
-    );
-  }
-  
-  
+  const cardClassName = `bg-glass-bg rounded-xl shadow-glass backdrop-blur border border-neumo-border ${small ? 'p-3 m-2' : 'p-6'}`;
+
+  return (
+    <div className={cardClassName}>
+      {children}
+    </div>
+  );
+}
 
 export default NeumoCard;
