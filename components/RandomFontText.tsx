53775ee786077004b4a3bbf8f54cb6a897962d33
ootami <ootani-yoshiyuki19@iamas.ac.jp>
2023-09-18 09:43:07 +0900

    style fixed

--- components/RandomFontText.tsx
+++ components/RandomFontText.tsx
@@ -20,19 +20,18 @@ const getRandomFont = () => {
     return fontList[Math.floor(Math.random() * fontList.length)];
   };
   
-const isMobile = () => {
+  const isMobile = () => {
     if (typeof window !== 'undefined') {
-        return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+      return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     }
     return false;
-    };
+  };
   
   interface RandomFontTextProps {
     children: string;
   }
   
   const RandomFontText: React.FC<RandomFontTextProps> = ({ children }) => {
-    const [font, setFont] = useState<string>(getRandomFont());
     const [hovered, setHovered] = useState(false);
     const ref = useRef<HTMLDivElement | null>(null);
   
@@ -43,17 +42,13 @@ const isMobile = () => {
           const viewportHeight = window.innerHeight;
           const isAtCenter = entry.boundingClientRect.top < viewportHeight / 2 && entry.boundingClientRect.bottom > viewportHeight / 2;
   
-          if (entry.isIntersecting && isAtCenter) {
-              setFont('Noto Sans, sans-serif');
-          } else {
-              setFont(getRandomFont());
-          }
+          setHovered(isAtCenter);
         };
   
         const observerOptions = {
           root: null,
           rootMargin: '0px',
-          threshold: 0.1, // Adjust as needed
+          threshold: [0.4, 0.6],
         };
   
         const observer = new IntersectionObserver(handleIntersection, observerOptions);
@@ -78,7 +73,7 @@ const isMobile = () => {
     };
   
     const characters = children.split('').map((char, index) => {
-      const currentFont = isMobile() ? font : (hovered ? 'Noto Sans, sans-serif' : getRandomFont());
+      const currentFont = hovered ? 'Noto Sans, sans-serif' : getRandomFont();
       return (
         <span key={index} style={{ fontFamily: currentFont }}>
           {char}
